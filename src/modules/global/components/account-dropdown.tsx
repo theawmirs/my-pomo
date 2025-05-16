e00c4c53551476885c32d61e6efd3457c1eafd62
Awmir <alendolen80@gmail.com>
2025-05-16 17:07:21 +0330

    chore: restore prisma generate in build and start scripts for consistent deployment process

--- src/modules/global/components/account-dropdown.tsx
+++ src/modules/global/components/account-dropdown.tsx
@@ -13,7 +13,7 @@ import DarkmodeToggle from "./theme-toggle";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/modules/ui-components/shadcn/ui/avatar";
 import { signOut } from "@/lib/auth/auth";
-import { Badge } from "@/modules/ui-components/shadcn/ui/badge";
+// import { Badge } from "@/modules/ui-components/shadcn/ui/badge";
 const AccountDropdown = async () => {
   const session = await auth();
   return (
