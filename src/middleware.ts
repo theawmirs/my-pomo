b9ea6996afcde0a5ccfac310561a8d88bfdd32f7
Awmir <alendolen80@gmail.com>
2025-06-05 12:00:05 +0330

    chore: remove unused middleware file for cleaner codebase

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+export { auth as middleware } from "@/lib/auth/auth";
+
+export const config = {
+  // Match all request paths except for the ones starting with:
+  // - api (API routes)
+  // - _next/static (static files)
+  // - _next/image (image optimization files)
+  // - favicon.ico (favicon file)
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
